6f78935c4d4c854a07532cda1c81bf0f1ba63de3
Bikcode <bikcodeh@gmail.com>
2023-11-26 07:37:08 -0500

    tests for gif expert app

---
+++ tests/GifExpertApp.test.jsx
@@ -0,0 +1,93 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import GifExpertApp from '../src/GifExpertApp';
+import { useFetchGifs } from '../src/hooks/useFetchGifs';
+
+jest.mock("../src/hooks/useFetchGifs");
+
+describe('Tests for GifExpertApp', () => { 
+
+    test('should show items after initialized', () => { 
+        const gifs = [
+            {
+                id: "BAC",
+                title: "test",
+                url: "https://imagen.jpg"
+            },
+            {
+                id: "123",
+                title: "test",
+                url: "https://imagen.jpg"
+            }
+        ]
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+        render(<GifExpertApp />);
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+        const categories = screen.getAllByRole('heading');
+        fireEvent.input(input, { target: { value: "Saitama" } });
+        fireEvent.submit(form);
+        expect(categories.length).toBe(2);
+     });
+
+
+    test('should not add repeated category', () => { 
+        const gifs = [
+            {
+                id: "BAC",
+                title: "test",
+                url: "https://imagen.jpg"
+            },
+            {
+                id: "123",
+                title: "test",
+                url: "https://imagen.jpg"
+            }
+        ]
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+        render(<GifExpertApp />);
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+        const categories = screen.getAllByRole('heading');
+        fireEvent.input(input, { target: { value: "Saitama" } });
+        fireEvent.submit(form);
+        fireEvent.input(input, { target: { value: "Saitama" } });
+        fireEvent.submit(form);
+        expect(categories.length).toBe(2);
+        screen.debug()
+     });
+
+
+    test('should add new repeated category', () => { 
+        const gifs = [
+            {
+                id: "BAC",
+                title: "test",
+                url: "https://imagen.jpg"
+            },
+            {
+                id: "123",
+                title: "test",
+                url: "https://imagen.jpg"
+            }
+        ]
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+        render(<GifExpertApp />);
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+        fireEvent.input(input, { target: { value: "Saitama" } });
+        fireEvent.submit(form);
+        fireEvent.input(input, { target: { value: "Mob" } });
+        fireEvent.submit(form);
+        const categories = screen.getAllByRole('heading');
+        expect(categories.length).toBe(4);
+     });
+ });
